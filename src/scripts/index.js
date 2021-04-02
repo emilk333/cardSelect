8ab2daa1c435fdff28e55e074a16b2a5f1cd1eec
emilk333 <emil.wtf@gmail.com>
2021-04-02 02:46:24 +0200

    final touches for mvp1

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,8 +9,11 @@ let sliderButtonToggleState = false
 function applyGlobalTheme(styleId) {
 	const themeStyleClass = `${styleId}`
 	const themeContainerElement = document.querySelector("#theme-container")
-	themeContainerElement.className = ""
-	themeContainerElement.classList.add(themeStyleClass)
+
+	if (themeContainerElement) {
+		themeContainerElement.className = ""
+		themeContainerElement.classList.add(themeStyleClass)
+	}
 }
 
 function applyThemeAccordingToCardTarget(event, cardElements) {
@@ -24,14 +27,50 @@ function applyThemeAccordingToCardTarget(event, cardElements) {
 function toggleSliderButtonTextColor(type) {
 	const monthlyText = document.querySelector(".sig-options__slider-text-monthly")
 	const annuallyText = document.querySelector(".sig-options__slider-text-annually")
+	const colorClass = 'sig-options__slider-text-color'
+	const noColorClass = 'sig-options__slider-text-no-color'
+	
+	if (type === 'annually' && monthlyText) {	
+		monthlyText.classList.add(colorClass)
+	} else {
+		monthlyText.classList.remove(colorClass)
+	}
+
+	if (type === 'monthly' && annuallyText) {	
+		annuallyText.classList.remove(noColorClass)
+	} else {
+		annuallyText.classList.add(noColorClass)
+	}
+}
+
+function applyBillingOptionsLabels(type) {
+	const monthlyLabel = 'per month'
+	const annuallyLabel = 'yearly'
+	const priceElementLabels = document.querySelectorAll(".sig-card__price-label")
+
+	priceElementLabels.forEach(node => {
+		node.innerText = type === 'monthly' ? monthlyLabel : annuallyLabel
+	})
+}
+
+function applyNewPrice(type) {
+	const teamPriceElement = document.querySelector(".sig-card__price--team")
+	const tribePriceElement = document.querySelector(".sig-card__price--tribe")
+	const annuallyPriceTeam = '$299.99'
+	const monthlyPriceTeam = '$29.99'
+	const annuallyPriceTribe = '$999.99'
+	const monthlyPriceTribe = '$149.99'
 	
 	if (type === 'monthly') {
-		monthlyText.style.color = "red"
-		annuallyText.style.color = "#fff"
-	} 
+		teamPriceElement.innerText = monthlyPriceTeam
+		tribePriceElement.innerText = monthlyPriceTribe
+		applyBillingOptionsLabels(type)
+	}
+
 	if (type === 'annually') {
-		monthlyText.style.color = "#fff"
-		annuallyText.style.color = "red"
+		teamPriceElement.innerText = annuallyPriceTeam
+		tribePriceElement.innerText = annuallyPriceTribe
+		applyBillingOptionsLabels(type)
 	}
 }
 
@@ -43,10 +82,12 @@ function toggleBillingOptions(sliderButtonElement) {
 	
 	if (sliderButtonToggleState) {
 		elemToApplyClass.classList.add(shiftButtonLeftClass)
-		toggleSliderButtonTextColor('monthly')
+		toggleSliderButtonTextColor('annually')
+		applyNewPrice('annually')
 	} else {
 		elemToApplyClass.classList.remove(shiftButtonLeftClass)
-		toggleSliderButtonTextColor('annually')
+		toggleSliderButtonTextColor('monthly')
+		applyNewPrice('monthly')
 	}
 }
 
@@ -54,13 +95,17 @@ function run() {
 	const cardElements = document.querySelectorAll(".sig-card")
 	const sliderButtonElement = document.querySelector("#slider-button-billing-option")
 
-	sliderButtonElement.addEventListener('click', function(e) {
-		toggleBillingOptions(sliderButtonElement)
-	});
+	if (sliderButtonElement) {
+		sliderButtonElement.addEventListener('click', function(e) {
+			toggleBillingOptions(sliderButtonElement)
+		});
+	}
 
-	cardElements.forEach(card => card.addEventListener('click', function(e) {
-		applyThemeAccordingToCardTarget(e, cardElements)
-	}));
+	if (cardElements) {
+		cardElements.forEach(card => card.addEventListener('click', function(e) {
+			applyThemeAccordingToCardTarget(e, cardElements)
+		}));
+	}
 }
 
 
